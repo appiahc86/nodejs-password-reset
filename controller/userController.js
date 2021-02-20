40744f4a9055d0e7a979c13f5eef337aeddf9ebb
Appiah Collins <54290051+appiahc86@users.noreply.github.com>
2021-02-20 04:31:10 +0000

    Update userController.js
    updated

--- controller/userController.js
+++ controller/userController.js
@@ -154,8 +154,7 @@ let mailOptions = {
                     host: process.env.EMAIL_HOST,
                     port: process.env.EMAIL_PORT,
                     auth: {
-                        // user: 'wsappiah@gmail.com',
-                        // pass: 'innocent360'
+                       
                         user: process.env.EMAIL_USER,
                         pass: process.env.EMAIL_PASS
                     }
